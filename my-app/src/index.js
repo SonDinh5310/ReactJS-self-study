9aad3e7c22cb79b34a74071f9784ec70cf56a8ca
Sondinh <dnson20022001@gmail.com>
2021-01-21 23:50:45 +0700

    commit code]

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -47,26 +47,49 @@ import ReactDom from "react-dom";
 //     );
 // };
 
+//* CSS
+import "./index.css";
+//* Setup
+const firstBook = {
+    img:
+        "https://images-na.ssl-images-amazon.com/images/I/41L5qgUW2fL._AC_SX184_.jpg",
+    author: "Barack Obama",
+    title: "A Promised Land",
+};
+const secondBook = {
+    img:
+        "https://images-na.ssl-images-amazon.com/images/I/81h2gWPTYJL._AC_UL320_SR210,320_.jpg",
+    author: "Michelle Obama",
+    title: "Becoming",
+};
+
 function BookList() {
     return (
-        <section>
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
+        <section className="booklist">
+            <Book
+                img={firstBook.img}
+                title={firstBook.title}
+                author={firstBook.author}
+            />
+            <Book
+                img={secondBook.img}
+                title={secondBook.title}
+                author={secondBook.author}
+            />
         </section>
     );
 }
 
-const Book = () => {
-    return <article>this is a booklist</article>;
+const Book = (props) => {
+    const { img, title, author } = props;
+    console.log(props);
+    return (
+        <article className="book">
+            <img src={img} alt="" />
+            <h1>{title}</h1>
+            <h4>{author}</h4>
+        </article>
+    );
 };
-
 ReactDom.render(<BookList />, document.getElementById("root"));
 //1h39m
